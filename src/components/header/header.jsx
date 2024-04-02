aee3032b3451754718a1c2d96c0e12e4f5e0259e
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-04-02 22:00:58 +0100

    :lipstick: feat: change the styles of the header component elements

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,22 +3,30 @@ import schoolLogo from '../../assets/aesa-logo.png';
 
 export const Header = () => {
   return (
-    <header className='flex items-center justify-between border-b-2 border-green-100 rounded-b-md'>
-      <Link to={'/'}>
-        <div id='logo-div' className='flex items-center space-x-5'>
+    <header className='pr-5 flex items-center justify-between border-b-2 border-zinc-100 shadow'>
+      <Link to={'/'} className='flex items-center space-x-8'>
+        <div id='logo-div' className="flex items-center justify-center w-[10rem] bg-zinc-800 rounded-r-md">
           <img 
             src={schoolLogo} 
             alt="School Logo"
-            className="object-scale-down max-h-full w-24 h-24 p-2"
+            className="object-scale-down max-h-full w-24 h-24 p-2 rounded-r-md"
           />
-          <h1 className="text-[1.7rem] text-zinc-700">Título do Site</h1>
         </div>
+        <h1 className="text-[1.7rem] text-zinc-700">Título do Site</h1>
       </Link>
-      <nav id='header-menu' className='px-2'>
-        <a href="#" className='p-3 mx-3 text-[1.1rem] border-b-2 border-transparent text-zinc-700 transition ease-linear hover:border-green-300 hover:text-green-400'>Link 1</a>
-        <a href="#" className='p-3 mx-3 text-[1.1rem] border-b-2 border-transparent text-zinc-700 transition ease-linear hover:border-green-300 hover:text-green-400'>Link 2</a>
-        <a href="#" className='p-3 mx-3 text-[1.1rem] border-b-2 border-transparent text-zinc-700 transition ease-linear hover:border-green-300 hover:text-green-400'>Link 3</a>
-        <a href="#" className='p-3 mx-3 text-[1.1rem] border-b-2 border-transparent text-zinc-700 transition ease-linear hover:border-green-300 hover:text-green-400'>Sobre</a>
+      <nav id='header-menu' className='px-2 space-x-3'>
+        <a 
+          href="/" 
+          className='p-3 text-[1.1rem] border-b-2 border-transparent text-zinc-700 
+          transition ease-linear hover:border-green-300 hover:text-green-400'>Início</a>
+        <a 
+          href="#" 
+          className='p-3 text-[1.1rem] border-b-2 border-transparent text-zinc-700 
+          transition ease-linear hover:border-green-300 hover:text-green-400'>Conteúdos</a>
+        <a 
+          href="#" 
+          className='p-3 text-[1.1rem] border-b-2 border-transparent text-zinc-700 
+          transition ease-linear hover:border-green-300 hover:text-green-400'>Sobre</a>
       </nav>
     </header>
   );
