e955620f701f57d6549a1e8b05a5b95289a41fbe
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-04-03 12:48:34 +0100

    :wrench: config: define own settings for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
   export default {
     content: ["./src/**/*.{html,js,jsx}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'school-pattern': "url('./src/assets/school-image.jpg')",
+        },
+        colors: {
+          light_black: "rgb(0, 0, 0, 0.850)",
+          light_black_two: "rgb(0, 0, 0, 0.950)",
+          light_black_thr: "rgb(0, 0, 0, 0.700)",
+        },
+      },  
     },
     plugins: [],
   }
