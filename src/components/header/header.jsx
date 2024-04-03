6ac48f59c987321135e8c39c6fb32088e1988d88
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-04-03 12:50:17 +0100

    :sparkles: feat: change the header elements

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,16 +3,16 @@ import schoolLogo from '../../assets/aesa-logo.png';
 
 export const Header = () => {
   return (
-    <header className='pr-5 flex items-center justify-between border-b-2 border-zinc-100 bg-zinc-800 shadow'>
+    <header className='pr-5 flex items-center justify-between bg-light_black_two border-b-4 border-green-600 shadow'>
       <div className='flex items-center space-x-5'>
-        <div className="flex items-center justify-center w-[8rem] bg-green-600 rounded-r-md">
+        <div className="flex items-center justify-center w-[8rem] bg-green-600">
           <img 
             src={schoolLogo} 
             alt="School Logo"
-            className="object-scale-down max-h-full w-24 h-24 p-2 rounded-r-md"
+            className="object-scale-down max-h-full w-20 h-20 p-2 rounded-r-md"
           />
         </div>
-        <h1 className="text-[1.7rem] text-zinc-50">Título do Website</h1>
+        <h1 className="text-[1.7rem] text-zinc-50">TECIAESA</h1>
       </div>
         <nav id='header-menu' className='px-2 space-x-5'>
           <a 
@@ -24,7 +24,7 @@ export const Header = () => {
             className='p-2 text-[1.1rem] border-b-2 border-transparent text-zinc-50 
             transition ease-linear hover:border-green-600 hover:text-green-600'>Conteúdos</a>
           <a 
-            href="#" 
+            href="/aboutus" 
             className='p-2 text-[1.1rem] border-b-2 border-transparent text-zinc-50 
             transition ease-linear hover:border-green-600 hover:text-green-600'>Sobre</a>
         </nav>
