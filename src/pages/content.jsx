cc71122c7faf374f79b6bf5174cfcd682b6a1eff
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-04-05 12:20:57 +0100

    :sparkles: feat: create the contents page

---
+++ src/pages/content.jsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/header/header";
+
+const Content = () => {
+    return (
+        <>
+        <div className="bg-school-pattern bg-repeat-round bg-fixed">
+            <Header />
+        </div>
+        </>
+    );
+};
+
+export default Content;
