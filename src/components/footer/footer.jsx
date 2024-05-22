f99d2e52e5c8909163b13367a1605b09a2504d0d
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-22 12:21:42 +0100

    :sparkles: feat: import 'Link' from react-router-dom

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import logoSchool from "../../assets/aesa-logo.png";
 
 export const Footer = () => {
@@ -11,7 +12,7 @@ export const Footer = () => {
       </section>
       <section className="flex flex-row-reverse items-center justify-between space-x-5 px-5 bg-zinc-900 rounded-t-lg">
         <nav id='footer-menu' className='py-5 space-x-5'>
-        <Link to={'/'} className='p-2 text-[1.1rem] border-b-2 border-transparent text-zinc-50 
+          <Link to={'/'} className='p-2 text-[1.1rem] border-b-2 border-transparent text-zinc-50 
             transition ease-linear hover:border-green-600 hover:text-green-600'>
           Início
           </Link>
