701adf68ac16f778cb3c5a7f47349723d27e7d20
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-24 10:59:14 +0100

    :sparkles: feat: create a route to the form page

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,6 @@
 import Home from "./pages/home";
 import About from "./pages/about";
+import Quizz from "./pages/quizz";
 import Content from "./pages/content";
 import Project from "./pages/projects";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -12,6 +13,7 @@ export const Router = () => {
         <Route path="/aboutus" element={<About />} />
         <Route path="/content" element={<Content />} />
         <Route path="/projects" element={ <Project /> } />
+        <Route path="/quizz" element={ <Quizz /> } />
       </Routes>
     </BrowserRouter>
   );
